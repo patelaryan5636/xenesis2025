4a9dc2881cda7b694e6cbb63134e9e7fa85fcdeb
aminmihir01 <mihiramin99@gmail.com>
2025-01-27 12:23:51 +0530

    Update xenesis2025.sql
    drop and replace database with organizer table

--- xenesis2025.sql
+++ xenesis2025.sql
@@ -11,8 +11,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-
+DROP DATABASE IF EXISTS `xenesis2025`;
+CREATE DATABASE `xenesis2025`;
+USE `xenesis2025`;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,35 +30,35 @@ SET time_zone = "+00:00";
 -- Table structure for table `event_master`
 --
 
--- CREATE TABLE `event_master` (
---   `event_id` int(11) NOT NULL,
---   `event_name` varchar(20) NOT NULL,
---   `tagline` text NOT NULL,
---   `department_name` varchar(20) NOT NULL,
---   `team_name` varchar(20) NOT NULL,
---   `participation_price` int(11) NOT NULL,
---   `participation_price_team` int(11) NOT NULL,
---   `team_member_count` int(11) NOT NULL,
---   `winner_price1` int(11) NOT NULL,
---   `winner_price2` int(11) NOT NULL,
---   `winner_price3` int(11) NOT NULL,
---   `location` varchar(20) NOT NULL,
---   `date` date NOT NULL,
---   `event_description` text NOT NULL,
---   `rules` text NOT NULL,
---   `round1_title` varchar(50) NOT NULL,
---   `round1_description` text DEFAULT NULL,
---   `round2_title` varchar(50) NOT NULL,
---   `round2_description` text DEFAULT NULL,
---   `round3_title` varchar(50) NOT NULL,
---   `round3_description` text DEFAULT NULL,
---   `round4_title` varchar(50) DEFAULT NULL,
---   `round4_description` text DEFAULT NULL,
---   `round5_title` varchar(50) DEFAULT NULL,
---   `round5_description` text DEFAULT NULL,
---   `images` varchar(50) NOT NULL,
---   `max_tickets` int(11) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `event_master` (
+  `event_id` int(11) NOT NULL,
+  `event_name` varchar(20) NOT NULL,
+  `tagline` text NOT NULL,
+  `department_name` varchar(20) NOT NULL,
+  `team_name` varchar(20) NOT NULL,
+  `participation_price` int(11) NOT NULL,
+  `participation_price_team` int(11) NOT NULL,
+  `team_member_count` int(11) NOT NULL,
+  `winner_price1` int(11) NOT NULL,
+  `winner_price2` int(11) NOT NULL,
+  `winner_price3` int(11) NOT NULL,
+  `location` varchar(20) NOT NULL,
+  `date` date NOT NULL,
+  `event_description` text NOT NULL,
+  `rules` text NOT NULL,
+  `round1_title` varchar(50) NOT NULL,
+  `round1_description` text DEFAULT NULL,
+  `round2_title` varchar(50) NOT NULL,
+  `round2_description` text DEFAULT NULL,
+  `round3_title` varchar(50) NOT NULL,
+  `round3_description` text DEFAULT NULL,
+  `round4_title` varchar(50) DEFAULT NULL,
+  `round4_description` text DEFAULT NULL,
+  `round5_title` varchar(50) DEFAULT NULL,
+  `round5_description` text DEFAULT NULL,
+  `images` varchar(50) NOT NULL,
+  `max_tickets` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -65,17 +66,17 @@ SET time_zone = "+00:00";
 -- Table structure for table `user_master`
 --
 
--- CREATE TABLE `user_master` (
---   `user_id` int(11) NOT NULL,
---   `user_name` varchar(12) NOT NULL,
---   `email` varchar(30) NOT NULL,
---   `password` varchar(12) NOT NULL,
---   `user_role` int(11) NOT NULL DEFAULT 3,
---   `joined_date` datetime NOT NULL DEFAULT current_timestamp(),
---   `full_name` varchar(25) NOT NULL,
---   `phone` bigint(10) NOT NULL,
---   `isVerified` int(11) NOT NULL DEFAULT 0
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `user_master` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(12) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `user_role` int(11) NOT NULL DEFAULT 3,
+  `joined_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `full_name` varchar(25) NOT NULL,
+  `phone` bigint(10) NOT NULL,
+  `isVerified` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 CREATE TABLE `organizer_master` (
