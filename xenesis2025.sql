95881b758f4f37ee11880ef5a6eb7ca39bfaae3e
Sachani <patelaryan5636@gmail.com>
2025-01-27 10:15:03 +0530

    Create xenesis2025.sql

---
+++ xenesis2025.sql
@@ -0,0 +1,119 @@
++-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 26, 2025 at 11:15 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+DROP DATABASE IF EXISTS `xenesis2025`;
+CREATE DATABASE `xenesis2025`;
+USE `xenesis2025`;
+
+
+--
+-- Database: `xenesis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_master`
+--
+
+CREATE TABLE `event_master` (
+  `event_id` int(11) NOT NULL,
+  `event_name` varchar(20) NOT NULL,
+  `team_name` varchar(20) NOT NULL,
+  `event_price` int(11) NOT NULL,
+  `event_location` text NOT NULL,
+  `event_date` date NOT NULL,
+  `event_description` text NOT NULL,
+  `tagline` text NOT NULL,
+  `poster_image` text NOT NULL,
+  `is_teamevent` int(11) NOT NULL DEFAULT 0,
+  `team_size` int(11) NOT NULL,
+  `event_size` int(11) NOT NULL,
+  `is_closed` int(11) NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 1,
+  `team_leader_id` int(11) NOT NULL,
+  `round1` text NOT NULL,
+  `round2` text NOT NULL,
+  `round3` text NOT NULL,
+  `rules` text NOT NULL,
+  `winner_price1` int(11) NOT NULL,
+  `winner_price2` int(11) NOT NULL,
+  `round1_title` text NOT NULL,
+  `round2_title` text NOT NULL,
+  `round3_title` text NOT NULL,
+  `team_price` int(11) NOT NULL,
+  `min_team_participant_count` int(11) NOT NULL,
+  `max_tickets` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_master`
+--
+
+CREATE TABLE `user_master` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(12) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `user_role` int(11) NOT NULL DEFAULT 3,
+  `joined_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `full_name` varchar(25) NOT NULL,
+  `phone` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `event_master`
+--
+ALTER TABLE `event_master`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `user_master`
+--
+ALTER TABLE `user_master`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `event_master`
+--
+ALTER TABLE `event_master`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_master`
+--
+ALTER TABLE `user_master`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
