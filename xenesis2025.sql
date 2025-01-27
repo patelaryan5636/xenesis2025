e8b1f08181b83a5aee16d29bb7ccc05517c5de16
aminmihir01 <mihiramin99@gmail.com>
2025-01-27 12:21:11 +0530

    Update xenesis2025.sql
    organizer_table

--- xenesis2025.sql
+++ xenesis2025.sql
@@ -29,35 +29,35 @@ SET time_zone = "+00:00";
 -- Table structure for table `event_master`
 --
 
-CREATE TABLE `event_master` (
-  `event_id` int(11) NOT NULL,
-  `event_name` varchar(20) NOT NULL,
-  `tagline` text NOT NULL,
-  `department_name` varchar(20) NOT NULL,
-  `team_name` varchar(20) NOT NULL,
-  `participation_price` int(11) NOT NULL,
-  `participation_price_team` int(11) NOT NULL,
-  `team_member_count` int(11) NOT NULL,
-  `winner_price1` int(11) NOT NULL,
-  `winner_price2` int(11) NOT NULL,
-  `winner_price3` int(11) NOT NULL,
-  `location` varchar(20) NOT NULL,
-  `date` date NOT NULL,
-  `event_description` text NOT NULL,
-  `rules` text NOT NULL,
-  `round1_title` varchar(50) NOT NULL,
-  `round1_description` text DEFAULT NULL,
-  `round2_title` varchar(50) NOT NULL,
-  `round2_description` text DEFAULT NULL,
-  `round3_title` varchar(50) NOT NULL,
-  `round3_description` text DEFAULT NULL,
-  `round4_title` varchar(50) DEFAULT NULL,
-  `round4_description` text DEFAULT NULL,
-  `round5_title` varchar(50) DEFAULT NULL,
-  `round5_description` text DEFAULT NULL,
-  `images` varchar(50) NOT NULL,
-  `max_tickets` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- CREATE TABLE `event_master` (
+--   `event_id` int(11) NOT NULL,
+--   `event_name` varchar(20) NOT NULL,
+--   `tagline` text NOT NULL,
+--   `department_name` varchar(20) NOT NULL,
+--   `team_name` varchar(20) NOT NULL,
+--   `participation_price` int(11) NOT NULL,
+--   `participation_price_team` int(11) NOT NULL,
+--   `team_member_count` int(11) NOT NULL,
+--   `winner_price1` int(11) NOT NULL,
+--   `winner_price2` int(11) NOT NULL,
+--   `winner_price3` int(11) NOT NULL,
+--   `location` varchar(20) NOT NULL,
+--   `date` date NOT NULL,
+--   `event_description` text NOT NULL,
+--   `rules` text NOT NULL,
+--   `round1_title` varchar(50) NOT NULL,
+--   `round1_description` text DEFAULT NULL,
+--   `round2_title` varchar(50) NOT NULL,
+--   `round2_description` text DEFAULT NULL,
+--   `round3_title` varchar(50) NOT NULL,
+--   `round3_description` text DEFAULT NULL,
+--   `round4_title` varchar(50) DEFAULT NULL,
+--   `round4_description` text DEFAULT NULL,
+--   `round5_title` varchar(50) DEFAULT NULL,
+--   `round5_description` text DEFAULT NULL,
+--   `images` varchar(50) NOT NULL,
+--   `max_tickets` int(11) NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -65,18 +65,40 @@ CREATE TABLE `event_master` (
 -- Table structure for table `user_master`
 --
 
-CREATE TABLE `user_master` (
-  `user_id` int(11) NOT NULL,
-  `user_name` varchar(12) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `password` varchar(12) NOT NULL,
-  `user_role` int(11) NOT NULL DEFAULT 3,
-  `joined_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `full_name` varchar(25) NOT NULL,
-  `phone` bigint(10) NOT NULL,
-  `isVerified` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- CREATE TABLE `user_master` (
+--   `user_id` int(11) NOT NULL,
+--   `user_name` varchar(12) NOT NULL,
+--   `email` varchar(30) NOT NULL,
+--   `password` varchar(12) NOT NULL,
+--   `user_role` int(11) NOT NULL DEFAULT 3,
+--   `joined_date` datetime NOT NULL DEFAULT current_timestamp(),
+--   `full_name` varchar(25) NOT NULL,
+--   `phone` bigint(10) NOT NULL,
+--   `isVerified` int(11) NOT NULL DEFAULT 0
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
+CREATE TABLE `organizer_master` (
+  `Leader_Name` varchar(20) NOT NULL,
+  `Leader_email` varchar(25) NOT NULL,
+  `Leader_mobile` int(10) NOT NULL,
+  `Member1_name` varchar(20) NOT NULL,
+  `Member1_email` varchar(25) NOT NULL,
+  `Member1_mobile` int(10) NOT NULL,
+  `Member2_name` varchar(20) NOT NULL,
+  `Member2_email` varchar(25) NOT NULL,
+  `Member2_mobile` int(10) NOT NULL,
+  `Member3_name` varchar(20) NOT NULL,
+  `Member3_email` varchar(25) NOT NULL,
+  `Member3_mobile` int(10) NOT NULL,
+  `Member4_name` varchar(20) NOT NULL,
+  `Member4_email` varchar(25) NOT NULL,
+  `Member4_mobile` int(10) NOT NULL,
+  `Member5_name` varchar(20) NOT NULL,
+  `Member5_email` varchar(25) NOT NULL,
+  `Member5_mobile` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
 --
 -- Indexes for dumped tables
 --
