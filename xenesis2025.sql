ca12bc136136db986c5d870fb5d4c2de63368626
aminmihir01 <mihiramin99@gmail.com>
2025-01-28 13:34:29 +0530

    DEPARTMENT_TABLE

--- xenesis2025.sql
+++ xenesis2025.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 27, 2025 at 03:49 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Jan 28, 2025 at 09:02 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,18 @@ USE `xenesis2025`;
 --
 -- Database: `xenesis2025`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department_master`
+--
+
+CREATE TABLE `department_master` (
+  `department_id` int(11) NOT NULL,
+  `department_name` varchar(50) NOT NULL,
+  `poster_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
