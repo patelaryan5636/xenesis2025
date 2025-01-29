ab7bb367f43d29af664e371984c847ea846f9e08
Rangat001 <rgtwork114@gmail.com>
2025-01-29 15:06:46 +0530

    Database Update

--- xenesis2025.sql
+++ xenesis2025.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2025 at 05:25 PM
+-- Generation Time: Jan 29, 2025 at 10:32 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -11,7 +11,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
+DROP DATABASE IF EXISTS `xenesis2025`;
+CREATE DATABASE `xenesis2025`;
+USE `xenesis2025`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -43,7 +45,7 @@ CREATE TABLE `event_master` (
   `event_id` int(11) NOT NULL,
   `event_name` varchar(100) NOT NULL,
   `tagline` text NOT NULL,
-  `department_name` varchar(100) NOT NULL,
+  `department_id` int(11) NOT NULL,
   `team_name` varchar(100) NOT NULL,
   `participation_price` int(11) NOT NULL,
   `participation_price_team` int(11) NOT NULL,
@@ -67,15 +69,37 @@ CREATE TABLE `event_master` (
   `round5_description` text DEFAULT NULL,
   `images` varchar(254) NOT NULL,
   `image_path` text NOT NULL,
-  `max_tickets` int(11) NOT NULL
+  `max_tickets` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `event_master`
 --
 
-INSERT INTO `event_master` (`event_id`, `event_name`, `tagline`, `department_name`, `team_name`, `participation_price`, `participation_price_team`, `team_member_count`, `winner_price1`, `winner_price2`, `winner_price3`, `location`, `date`, `event_description`, `rules`, `round1_title`, `round1_description`, `round2_title`, `round2_description`, `round3_title`, `round3_description`, `round4_title`, `round4_description`, `round5_title`, `round5_description`, `images`, `image_path`, `max_tickets`) VALUES
-(3, 'asdf', 'asdfasd', 'asdf', 'aryan', 12, 11, 3, 12, 12, 12, 'adsf', '0000-00-00', 'asdfasdfsadfs', 'asdfasddfasdf', 'asdfasdfdsa', 'asdfasdf', 'asdfasdf', 'asdfasdf', 'asdfasf', 'asdfasfds', 'asdfasdf', 'asdfasdfsadf', 'adsfasdf', 'asdfasdf', 'http://drive.google.com/file/d/1A85_XHzhOtikBHuT2sFIwer40kpoqKAK/view?usp=sharing', 'uploads/1A85_XHzhOtikBHuT2sFIwer40kpoqKAK.jpg', 500);
+INSERT INTO `event_master` (`event_id`, `event_name`, `tagline`, `department_id`, `team_name`, `participation_price`, `participation_price_team`, `team_member_count`, `winner_price1`, `winner_price2`, `winner_price3`, `location`, `date`, `event_description`, `rules`, `round1_title`, `round1_description`, `round2_title`, `round2_description`, `round3_title`, `round3_description`, `round4_title`, `round4_description`, `round5_title`, `round5_description`, `images`, `image_path`, `max_tickets`) VALUES
+(3, 'asdf', 'asdfasd', 0, 'aryan', 12, 11, 3, 12, 12, 12, 'adsf', '0000-00-00', 'asdfasdfsadfs', 'asdfasddfasdf', 'asdfasdfdsa', 'asdfasdf', 'asdfasdf', 'asdfasdf', 'asdfasf', 'asdfasfds', 'asdfasdf', 'asdfasdfsadf', 'adsfasdf', 'asdfasdf', 'http://drive.google.com/file/d/1A85_XHzhOtikBHuT2sFIwer40kpoqKAK/view?usp=sharing', 'uploads/1A85_XHzhOtikBHuT2sFIwer40kpoqKAK.jpg', 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_master`
+--
+
+CREATE TABLE `group_master` (
+  `group_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `leader_id` varchar(14) NOT NULL,
+  `member_1` varchar(14) NOT NULL,
+  `member_2` varchar(14) NOT NULL,
+  `member_3` varchar(14) NOT NULL,
+  `member_4` varchar(14) NOT NULL,
+  `member_5` varchar(14) NOT NULL,
+  `member_6` varchar(14) NOT NULL,
+  `member_7` varchar(14) NOT NULL,
+  `is_confirmed` int(11) NOT NULL,
+  `confirmBy` varchar(14) NOT NULL,
+  `receipt_no` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -107,6 +131,21 @@ CREATE TABLE `organizer_master` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `participant_master`
+--
+
+CREATE TABLE `participant_master` (
+  `p_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `student_id` varchar(20) NOT NULL,
+  `is_confirmed` int(11) NOT NULL,
+  `confirmBy` varchar(20) NOT NULL,
+  `receipt_no` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_master`
 --
 
@@ -122,6 +161,17 @@ CREATE TABLE `user_master` (
   `isVerified` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `volunteer_master`
+--
+
+CREATE TABLE `volunteer_master` (
+  `volunteer_id` varchar(14) NOT NULL,
+  `wallet` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -131,6 +181,18 @@ CREATE TABLE `user_master` (
 --
 ALTER TABLE `event_master`
   ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `group_master`
+--
+ALTER TABLE `group_master`
+  ADD PRIMARY KEY (`group_id`);
+
+--
+-- Indexes for table `participant_master`
+--
+ALTER TABLE `participant_master`
+  ADD PRIMARY KEY (`p_id`);
 
 --
 -- Indexes for table `user_master`
@@ -149,6 +211,18 @@ ALTER TABLE `user_master`
 --
 ALTER TABLE `event_master`
   MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `group_master`
+--
+ALTER TABLE `group_master`
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `participant_master`
+--
+ALTER TABLE `participant_master`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user_master`
